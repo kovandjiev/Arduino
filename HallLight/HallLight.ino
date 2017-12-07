3cd7264cd89a790540c9312e55f49d17352699c4
Plamen Kovandjiev <p.kovandjiev@kmpelectronics.eu>
2017-12-07 22:06:02 +0200

    Add on, off and fade functionality

--- HallLight/HallLight.ino
+++ HallLight/HallLight.ino
@@ -13,34 +13,44 @@ const byte DIMMER_OUTPUT = 9;
 const int MAX_BRIGHTNESS = 255;
 const int MAX_BRIGHTNESS_NIGHT = 85;
 const int MAX_BRIGHTNESS_NO_POWER = 127;
-const int DELAY_LIGHTS_ON_MS = 60000; // One minute is on
+const unsigned long DELAY_LIGHTS_ON_MS = 10000; // One minute is on
+const unsigned long FADE_DELAY_ON_MS = 30;
+const unsigned long FADE_DELAY_OFF_MS = 100;
+const int FADE_POINTS = 5;    // how many points to fade the LED by
 
 bool _inputState[CHECK_INPUTS_LEN];
 
 int _brightness = 0;    // how bright the LED is
-int _fadeAmount = 5;    // how many points to fade the LED by
-unsigned long _onLighRemaining = 0;
+unsigned long _lightDelay = 0;
+unsigned long _fadeDelay = 0;
+int _maxBrightnes;
 
 void setup() 
 {
+	Serial.begin(115200);
+
 	for (byte i = 0; i < CHECK_INPUTS_LEN; i++)
 	{
-		pinMode(CHECK_INPUTS[i], INPUT);
+		pinMode(CHECK_INPUTS[i], INPUT_PULLUP);
 
 		// Initialize compare array.
 		_inputState[i] = digitalRead(CHECK_INPUTS[i]);
 	}
 	
-	pinMode(NO_POWER_INPUT, INPUT);
-	pinMode(NIGHT_INPUT, INPUT);
+	pinMode(NO_POWER_INPUT, INPUT_PULLUP);
+	pinMode(NIGHT_INPUT, INPUT_PULLUP);
 
 	pinMode(DIMMER_OUTPUT, OUTPUT);
 }
 
-void loop() {
+void loop() 
+{
+	setMaxBrightness();
+
 	bool inState = processInputs();
 
-	analogWrite(DIMMER_OUTPUT, 255);
+	procesBrightness(inState);
+	
 	//analogWrite(DIMMER_OUTPUT, _brightness);
 
 	// change the brightness for next time through the loop:
@@ -56,17 +66,67 @@ void loop() {
 
 void procesBrightness(bool state)
 {
+	unsigned long time = millis();
+
 	if (state)
 	{
-		_onLighRemaining = DELAY_LIGHTS_ON_MS;
+		Serial.println("State changed");
+		_lightDelay = time + DELAY_LIGHTS_ON_MS;
+		Serial.print("_lightDelay ");
+		Serial.println(_lightDelay);
+		Serial.print("time ");
+		Serial.println(time);
+		_fadeDelay = 0;
 	}
 
-	if (_onLighRemaining <= 0)
+	bool isOn = _lightDelay > time;
+
+	if (isOn)
 	{
+		if (_fadeDelay < time && _brightness < _maxBrightnes)
+		{
+			Serial.print("On ");
+			_brightness += FADE_POINTS;
+			_fadeDelay = time + FADE_DELAY_ON_MS;
+			Serial.println(_brightness);
+		}
+	}
+	else
+	{
+		if (_fadeDelay < time && _brightness > 0)
+		{
+			Serial.print("Off ");
+			_brightness -= FADE_POINTS;
+			_fadeDelay = time + FADE_DELAY_OFF_MS;
+			Serial.println(_brightness);
+		}
+	}
+
+	if (_brightness > _maxBrightnes)
+	{
+		_brightness = _maxBrightnes;
+	}
+
+	analogWrite(DIMMER_OUTPUT, _brightness);
+}
+
+void setMaxBrightness()
+{
+	if (digitalRead(NIGHT_INPUT) == HIGH)
+	{
+		_maxBrightnes = MAX_BRIGHTNESS_NIGHT;
+		
 		return;
 	}
 
+	if (digitalRead(NO_POWER_INPUT) == HIGH)
+	{
+		_maxBrightnes = MAX_BRIGHTNESS_NO_POWER;
 
+		return;
+	}
+
+	_maxBrightnes = MAX_BRIGHTNESS;
 }
 
 bool processInputs()
