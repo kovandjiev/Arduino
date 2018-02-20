3995dd6a37020a47712667f89fd3159a23ad20b0
Plamen Kovandjiev <p.kovandjiev@kmpelectronics.eu>
2018-02-20 20:34:05 +0200

    Adding watch dog and pin description

--- HallLight/HallLight.ino
+++ HallLight/HallLight.ino
@@ -2,21 +2,23 @@
 HallLight.ino
 Hall light project is for fade switch on or off lights in my hall.
 */
+#include <avr/wdt.h>
+
 #define CHECK_INPUTS_LEN 10
 #define DEBUG
 
 // These pins check for change state low, high.
 const byte CHECK_INPUTS[CHECK_INPUTS_LEN] = { A0, 15, 14, 16, 10, 2, 3, 4, 5, 6 };
-const byte NO_POWER_INPUT = 7;
-const byte NIGHT_INPUT = 8;
+const byte NO_POWER_INPUT = 7; // Pull down. 1 - has power, 0 - hasn't power
+const byte IS_NIGHT_INPUT = 8; // Pull down. 0 - day, 1 - night
 const byte DIMMER_OUTPUT = 9;
-const byte LIGHTS_IS_ON = A1;
+const byte LIGHTS_IS_ON_OUTPUT = A1; // 1 - the light is On, 0 - the lights is Off
 
 const int MAX_BRIGHTNESS = 255;
 const int MAX_BRIGHTNESS_NIGHT = 85;
 const int MAX_BRIGHTNESS_NO_POWER = 127;
 const unsigned long DELAY_LIGHTS_ON_MS = 60000; //5000; // 60000 One minute is on
-const unsigned long FADE_DELAY_ON_MS = 6; // 30
+const unsigned long FADE_DELAY_ON_MS = 8; // 30
 const unsigned long FADE_DELAY_OFF_MS = 100; // 100
 const int FADE_POINTS = 1;    // 5 how many points to fade the LED by
 
@@ -34,6 +36,9 @@ void setup()
 	Serial.println("Hall light project is started");
 #endif // DEBUG
 
+	// Wait for 30 seconds for the board has opportunity to flash a new sketch. If comment this row, it can flash only in ISP.
+	delay(30000);
+
 	for (byte i = 0; i < CHECK_INPUTS_LEN; i++)
 	{
 		pinMode(CHECK_INPUTS[i], INPUT);
@@ -43,14 +48,40 @@ void setup()
 	}
 	
 	pinMode(NO_POWER_INPUT, INPUT);
-	pinMode(NIGHT_INPUT, INPUT);
+	pinMode(IS_NIGHT_INPUT, INPUT);
 
 	pinMode(DIMMER_OUTPUT, OUTPUT);
-	pinMode(LIGHTS_IS_ON, OUTPUT);
+	pinMode(LIGHTS_IS_ON_OUTPUT, OUTPUT);
+
+	// Start MC Watchdog timer.
+	cli(); // disable all interrupts
+	wdt_reset(); // reset the WDT timer    
+				 /*
+				 WDTCSR configuration:
+				 WDIE = 1: Interrupt Enable
+				 WDE = 1 :Reset Enable
+				 WDP3 WDP2 WDP1 WDP0
+				 0 0 0 0 2K (2048) cycles 16 ms
+				 0 0 0 1 4K (4096) cycles 32 ms
+				 0 0 1 0 8K (8192) cycles 64 ms
+				 0 0 1 1 16K (16384) cycles 0.125 s
+				 0 1 0 0 32K (32768) cycles 0.25 s
+				 0 1 0 1 64K (65536) cycles 0.5 s
+				 0 1 1 0 128K (131072) cycles 1.0 s
+				 0 1 1 1 256K (262144) cycles 2.0 s
+				 1 0 0 0 512K (524288) cycles 4.0 s
+				 1 0 0 1 1024K (1048576) cycles 8.0 s    */
+				 /* Start timed sequence */
+	WDTCSR |= (1 << WDCE) | (1 << WDE);
+	// Set Watchdog settings - Reset Enable, cycles 1.0 s.
+	WDTCSR = (1 << WDE) | (0 << WDP3) | (1 << WDP2) | (1 << WDP1) | (0 << WDP0);
+	sei();
 }
 
 void loop() 
 {
+	wdt_reset();
+
 	bool inState = processInputs();
 
 	bool lighState = processState(inState);
@@ -61,7 +92,7 @@ void loop()
 
 	setBrightness(newBrightness);
 
-	digitalWrite(LIGHTS_IS_ON, _brightness > 0 ? HIGH : LOW);
+	digitalWrite(LIGHTS_IS_ON_OUTPUT, _brightness > 0 ? HIGH : LOW);
 }
 
 void setBrightness(int brightness)
@@ -140,7 +171,7 @@ bool processState(bool inState)
 int getMaxBrightness()
 {
 	// Night arrived - active High
-	if (digitalRead(NIGHT_INPUT) == HIGH)
+	if (digitalRead(IS_NIGHT_INPUT) == HIGH)
 	{
 		return MAX_BRIGHTNESS_NIGHT;
 	}
