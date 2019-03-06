68335acc4b4decfd16c02ffa89938bae32c16a33
Plamen Kovandjiev <p.kovandjiev@kmpelectronics.eu>
2019-03-06 16:07:07 +0200

    Add secrets

---
+++ Discharger/arduino_secrets.h
@@ -0,0 +1,9 @@
+// Wifi authentication settings.
+const char* SSID = "WiFiId";
+const char* SSID_PASSWORD = "WiFiPass";
+
+// MQTT server settings. 
+const char* MQTT_SERVER = "xxx.xxx.xxx.xxx"; // Change it with yours data.
+const int MQTT_PORT = 1883; // Change it with yours data.
+const char* MQTT_USER = "MqttUser"; // Change it with yours data.
+const char* MQTT_PASS = "MqttPass"; // Change it with yours data.
